60d9e26c6abcb271cc40bb2ff6e43d57efd05590
Andrii Lishchyna <andrej044@i.ua>
2022-08-07 14:18:35 +0200

    Initiated state for App component

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,42 @@ import AddTask from "./AddTask";
 import TaskList from "./TaskList";
 
 export default class App extends Component {
-
+        state = {
+            tasks:[
+                {
+                    id:0,
+                    text: 'zagrać wreszcie w Wiedżmina 3',
+                    date: "2023-02-15",
+                    finishDate: null,
+                    important: true,
+                    active: true,
+                },
+                {
+                    id:1,
+                    text: 'wyuczyć angielski',
+                    date: "2022-12-25",
+                    finishDate: null,
+                    important: true,
+                    active: false,
+                },
+                {
+                    id:2,
+                    text: 'pójśc na rezonans',
+                    date: "2022-08-12",
+                    finishDate: null,
+                    important: true,
+                    active: true,
+                },
+                {
+                    id:3,
+                    text: 'pójśc do lekarza',
+                    date: "2022-08-19",
+                    finishDate: null,
+                    important: true,
+                    active: false,
+                }
+                ]
+        }
   render() {
     return (
         <div>
