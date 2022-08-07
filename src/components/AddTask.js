ea4110f0ad5867bd0bc380883a24cbd8afb9d765
Andrii Lishchyna <andrej044@i.ua>
2022-08-07 13:46:51 +0200

    Create new component addTask

---
+++ src/components/AddTask.js
@@ -0,0 +1,9 @@
+import React,{Component} from "react";
+
+export default class AddTask extends Component{
+    render() {
+        return(
+            <div>Create new Task</div>
+        )
+    }
+}
