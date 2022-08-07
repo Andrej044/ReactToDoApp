15f6087d37b6ad5884b957b07034c4fbc176737a
Andrii Lishchyna <andrej044@i.ua>
2022-08-07 14:03:28 +0200

    Include AddTask, TaskList components to main App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
 
 export default class App extends Component {
 
   render() {
     return (
-        <div>Hello</div>
+        <div>
+          <AddTask/>
+          <TaskList/>
+        </div>
     )
   }
 }
