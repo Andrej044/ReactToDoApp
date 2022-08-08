e5e5b49092cdd11271aa9b8d6c0a74293f5f08ad
Andrii Lishchyna <andrej044@i.ua>
2022-08-08 13:06:39 +0200

    Add  transfer task from active list to done list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,7 +2,9 @@ import React from "react";
 import Task from "./Task";
 
 export default function TaskList(props){
-    const tasks = props.tasks.map(task =>
+    const active = props.tasks.filter(task => task.active);
+    const done = props.tasks.filter(task => !task.active);
+    const activeTasks = active.map(task =>
         <Task
             key = {task.id}
             id = {task.id}
@@ -11,16 +13,32 @@ export default function TaskList(props){
             deleteTask = {props.deleteTask}
             changeTaskStatus = {props.changeTaskStatus}
         />)
+    const doneTasks = done.map(task =>
+        <Task
+            key = {task.id}
+            id = {task.id}
+            text = {task.text}
+            date = {task.date}
+            deleteTask = {props.deleteTask}
+            changeTaskStatus = {props.changeTaskStatus}
+        />)
+
     return(
-        <div className="task-list-wrapper">
-            <h2>Zadanie do zrobienia</h2>
-        <ul className="task-list task-list_active">
-            {tasks}
-        </ul>
+        <div className="tasks-wrapper">
+            <h2>ToDo List</h2>
+            <div className="task-list-wrapper">
+                <h3 className="task-list__title">Zadanie do zrobienia <em>({active.length})</em></h3>
+                <ul className="task-list__list task-list_active">
+                    {active.length !== 0 ? activeTasks : <p>Brak zadań</p>}
+                </ul>
+            </div>
             <hr/>
-        <ul className = "task-list task-list_done">
-            <li>Zadanie zrobionę(0)</li>
-        </ul>
+            <div className="task-list-wrapper">
+                <h3 className="task-list__title">Zadanie zrobione <em>({done.length})</em></h3>
+                <ul className = "task-list__list task-list_done">
+                    {doneTasks.slice(0,3)}
+                </ul>
+            </div>
         </div>
     )
 }
