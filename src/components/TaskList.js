b5836227f6ab209b1fcf24b5a1f8ceffeb335073
Andrii Lishchyna <andrej044@i.ua>
2022-08-08 11:49:51 +0200

    Passes properties from App state below, to Task component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,15 +1,25 @@
 import React from "react";
 import Task from "./Task";
 
-export default function TaskList(){
+export default function TaskList(props){
+    const tasks = props.tasks.map(task =>
+        <Task
+            key = {task.id}
+            id = {task.id}
+            text = {task.text}
+            date = {task.date}
+            deleteTask = {props.deleteTask}
+            changeTaskStatus = {props.changeTaskStatus}
+        />)
     return(
         <div className="task-list-wrapper">
+            <h2>Zadanie do zrobienia</h2>
         <ul className="task-list task-list_active">
-            <Task/>
+            {tasks}
         </ul>
             <hr/>
         <ul className = "task-list task-list_done">
-            <li>Zadanie zrobionę</li>
+            <li>Zadanie zrobionę(0)</li>
         </ul>
         </div>
     )
