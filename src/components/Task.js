7c26ea82986f3a9d3516586c9be66576ec00dbdc
Andrii Lishchyna <andrej044@i.ua>
2022-08-08 11:07:36 +0200

    Add button for p. task-list__content

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 
-export default function Task(){
+export default function Task(props){
+    const {text, id, date} = props;
     return(
-        <li>New Task</li>
+        <li className="task-list__task">
+           <h3 className="task-list__title">Task {id}</h3>
+            <p className="task-list__content">
+                {text} - do <span>{date}</span>
+                <button>Zostało zrobione</button>
+                <button>X</button>
+            </p>
+        </li>
     )
 }
