e0f03a0416b0d9bf82f21d0b1263a9b68bf812a9
Andrii Lishchyna <andrej044@i.ua>
2022-08-10 22:30:03 +0200

    Add start structure

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,22 @@
 import React,{Component} from "react";
 
 export default class AddTask extends Component{
+    state={
+        text:"",
+        checked: true, 
+    }
     render() {
         return(
-            <div>Create new Task</div>
+            <div className="addTask">
+                <form className="addTask-form">
+                    <input id="inputTask" type="text" placeholder="dodaj zadanie" value={this.state.text}/>
+                    <input type="checkbox" id="important" checked={this.state.checked}/>
+                    <label htmlFor="important">Priorytet</label>
+                    <label htmlFor="date">Do kiedy zrobić</label>
+                    <input type="date" id="date" value="2022-08-10" min="2022-01-01" max="2023-12-31"/>
+                    <button>Dodaj</button>
+                </form>
+            </div>
         )
     }
 }
