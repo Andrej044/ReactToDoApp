8df6eaf233ead4cd55a2403df45f2638ef8d706f
Andrii Lishchyna <andrej044@i.ua>
2022-08-10 17:09:46 +0200

    Add condition for rerender component

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,15 +2,35 @@ import React from "react";
 
 
 export default function Task(props){
-    const {text, id, date, deleteTask, changeTaskStatus} = props;
-    return(
-        <li className="task-list__task">
-           <h3 className="task-list__title">Task {id}</h3>
-            <p className="task-list__content">
-                {text} - do <span>{date}</span>
-                <button onClick={changeTaskStatus.bind(this,id)}>Zostało zrobione</button>
-                <button onClick={deleteTask.bind(this,id)}>X</button>
-            </p>
-        </li>
-    )
+
+    const style = {
+        color:"red"
+    }
+    const {text, active, id, date, important} = props.tasks;
+    console.log(props.tasks)
+
+    if(active) {
+        return(
+            <li className="task-list__task">
+               <h3 className="task-list__title">Task {id}</h3>
+                <p className="task-list__content">
+                    {important? <span style={style}>{text}</span> : text} - do <span>{date}</span>
+                    {active ? <button onClick={props.changeTaskStatus.bind(this,id)}>Zostało zrobione</button> : null}
+                    <button onClick={props.deleteTask.bind(this,id)}>X</button>
+                </p>
+            </li>
+        )
+
+    }else {
+        return(
+            <li className="task-list__task">
+                <h3 className="task-list__title">Task {id}</h3>
+                <p className="task-list__content">
+                     {text} - do <span>{date}</span>
+                    <button onClick={props.deleteTask.bind(this,id)}>X</button>
+                </p>
+            </li>
+        )
+    }
+
 }
