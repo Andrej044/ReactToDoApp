1c1b50e7aaa0f11e3f6e556ab860a0dab94333e4
Andrii Lishchyna <andrej044@i.ua>
2022-08-11 12:55:37 +0200

    Add styles for the form component

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,5 +1,5 @@
 import React,{Component} from "react";
-
+import "./AddTask.css"
 export default class AddTask extends Component{
     state={
         text:"",
@@ -9,12 +9,21 @@ export default class AddTask extends Component{
         return(
             <div className="addTask">
                 <form className="addTask-form">
-                    <input id="inputTask" type="text" placeholder="dodaj zadanie" value={this.state.text}/>
-                    <input type="checkbox" id="important" checked={this.state.checked}/>
-                    <label htmlFor="important">Priorytet</label>
-                    <label htmlFor="date">Do kiedy zrobić</label>
-                    <input type="date" id="date" value="2022-08-10" min="2022-01-01" max="2023-12-31"/>
-                    <button>Dodaj</button>
+                    <section className="addTask-section">
+                        <h2 className="addTask__title">Dodaj zadanie i wybierz datę do kiedy zrobić.</h2>
+                        <p className="addTask__input">
+                            <input id="inputTask" type="text" placeholder="dodaj zadanie" value={this.state.text}/>
+                        </p>
+                       <p className="addTask__input addTask__priority">
+                            <input  type="checkbox" id="important" checked={this.state.checked}/>
+                            <label htmlFor="important">Priorytet</label>
+                       </p>
+                        <p className="addTask__input addTask__date">
+                            <label htmlFor="date">Do kiedy zrobić</label>
+                            <input type="date" id="date" value="2022-08-10" min="2022-01-01" max="2023-12-31"/>
+                        </p>
+                        <button className="addTask__button btn">Dodaj</button>
+                    </section>
                 </form>
             </div>
         )
